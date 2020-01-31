c775fd099b5dc88a7f4aa0dac2be430e1e97bf0f
thpa-subash <subashthapa901@gmail.com>
2020-01-31 16:15:14 +0545

    almost done

--- details/models.py
+++ details/models.py
@@ -10,18 +10,32 @@ class teacherDetails(models.Model):
     def __str__(self):
         return self.teachername
 
+class program(models.Model):
+    programs = models.CharField(max_length=30,default="None" )
+
+    def __str__(self):
+        return self.programs
+
 class semester(models.Model):
     semesters = models.CharField(max_length=30 ,default="None")
 
     def __str__(self):
         return self.semesters
 
+
+year =[("2000",2000),
+       ('2001',2001),
+       ('2002',2002),]
 class teacher(models.Model):
     teachername = models.ForeignKey(teacherDetails ,on_delete=models.CASCADE)
+    year = models.CharField(
+        max_length=7,
+        choices=year,
+        default="Select a Year",)
+    program = models.ForeignKey(program ,on_delete=models.CASCADE)
     teachesSemester=models.ForeignKey(semester ,on_delete=models.CASCADE)
     teachesSubjects = models.ForeignKey(subjects, on_delete=models.CASCADE)
-
-
+    courseComplete = models.IntegerField(blank=True,null=True)
     def __str__(self):
         return self.teachername.teachername
 # Create your models here.
--- details/views.py
+++ details/views.py
@@ -1,9 +1,65 @@
 from django.shortcuts import render
 from .models import *
-
+from django.db.models import Q
+from django.contrib import messages
 # Create your views here.
-def home(request,id):
-    teachers = teacher.objects.filter(teachername=id).order_by('teachesSemester')
-    name = teacherDetails.objects.get(pk = id)
-    print (name.teachername)
-    return render(request,'details/home.html',{"teacher":teachers,'name':name})
+
+def home(request):
+    if request.method == 'POST':
+        query = request.POST['q'].strip()
+        print (query)
+
+
+        if query :
+
+            lookups = Q(teachername__teachername__iexact=query)
+            lookup = Q(teachername__icontains=query)
+
+            results = teacher.objects.filter(lookups).order_by('year','teachesSemester')
+            result = teacherDetails.objects.filter(lookup).values('teachername')
+
+            if results:
+                teachernam = teacherDetails.objects.values("teachername")
+                data = {}
+                for ik in teachernam:
+                    for val in ik.values():
+                        data[val] = ""
+                return render(request,'details/index.html',{'se':results,'data':data})
+            else:
+                return render(request,'details/index.html',{'result':result})
+                messages.error(request,'Result  not found')
+        else:
+            messages.error(request,'Result  not found')
+    # teachers = teacher.objects.filter(teachername=id).order_by('year','teachesSemester')
+    # name = teacherDetails.objects.get(pk = id)
+    # print (name.teachername)
+    #return render(request,'details/index.html',{"teacher":teachers,'name':name})
+    teachernam = teacherDetails.objects.values("teachername")
+    data={}
+    for ik in teachernam:
+        for val in ik.values():
+
+
+            data[val]=""
+    print (data)
+    return render(request,'details/index.html',{"data":data,})
+
+def search(request):
+    if request.method == 'POST':
+        query = request.POST['q']
+
+
+        if query :
+
+            lookups = Q(teachername__icontains=query)
+
+            results = std_detail.objects.filter(lookups).order_by('teachername')
+            print(results)
+            if results:
+                return render(request,'students/search.html',{'se':results})
+            else:
+                messages.error(request,'Result  not found')
+        else:
+            return HttpResponseRedirect("/students/search/")
+
+    return render(request, 'details/index.html')
